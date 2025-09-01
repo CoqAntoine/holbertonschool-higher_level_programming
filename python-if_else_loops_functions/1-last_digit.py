9a396c7c7e60aa47eb4189fdc0538422c3673c1f
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 <11534@holbertonstudents.com>
2025-09-01 15:27:03 +0200

    fix sentences printed

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ else:
     unsigned_number = number
 mod = unsigned_number % 10
 if mod > 5:
-    print(f"Last digit of {number} is {mod} and greater than 5")
+    print(f"Last digit of {number} is {mod} and is greater than 5")
 elif mod == 0:
-    print(f"Last digit of {number} is {mod} and is zero")
+    print(f"Last digit of {number} is {mod} and is 0")
 else:
     print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
     
