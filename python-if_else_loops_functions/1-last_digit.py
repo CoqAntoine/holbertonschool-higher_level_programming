7de740328c2f8a1fc33e47bead3288e4be5c38d3
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 <11534@holbertonstudents.com>
2025-09-01 15:25:23 +0200

    add 1-last_digit.py file

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    unsigned_number = number * -1
+else:
+    unsigned_number = number
+mod = unsigned_number % 10
+if mod > 5:
+    print(f"Last digit of {number} is {mod} and greater than 5")
+elif mod == 0:
+    print(f"Last digit of {number} is {mod} and is zero")
+else:
+    print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
+    
