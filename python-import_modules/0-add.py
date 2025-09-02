fab5dac3d74ea2c031410fa6e631dcb07bc69f05
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 <11534@holbertonstudents.com>
2025-09-02 15:17:12 +0200

    add 0- file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import add_0
+a = 1
+b = 2
+print("{:d}".format(a) + " + " + "{:d}".format(b) + " = " + "{:d}".format(add_0.add(a,b)))
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+Python programating : import modules
