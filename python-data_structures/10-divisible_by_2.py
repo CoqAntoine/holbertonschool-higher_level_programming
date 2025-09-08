ea9f2c704a47a483ace759916a83a4f68d113570
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 <11534@holbertonstudents.com>
2025-09-08 14:47:36 +0200

    fix simplification of program

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = [True] * len(my_list)
-    for i in my_list:
-        if my_list[i] % 2 == 0:
-            new_list[i] = True
-        else:
-            new_list[i] = False
-    return new_list
+    return [x % 2 == 0 for x in my_list]
