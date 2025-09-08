a88660078d74f32e9a12ed065425e47aaa7f0b9c
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 <11534@holbertonstudents.com>
2025-09-08 11:28:56 +0200

    fix when sentence is empty

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_letter = sentence[0]
+    if len(sentence) == 0:
+        first_letter = None
+    else:
+        first_letter = sentence[0]
     result = length, first_letter
     return result
