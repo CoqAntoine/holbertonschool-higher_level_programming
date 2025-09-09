78c0f804e9838c40dd84fa3ff7e984882857006d
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 <11534@holbertonstudents.com>
2025-09-09 14:11:39 +0200

    fix condition when key is not in dictionnary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    temp = {k: v for k, v in a_dictionary.items() if k != key}
-    a_dictionary.clear()
-    a_dictionary.update(temp)
+    if key in a_dictionary:
+        temp = {k: v for k, v in a_dictionary.items() if k != key}
+        a_dictionary.clear()
+        a_dictionary.update(temp)
     return a_dictionary
-
