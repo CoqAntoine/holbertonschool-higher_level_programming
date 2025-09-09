bc9d90a1ed4c553381de42079a0a3c8c1eea2b71
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 <11534@holbertonstudents.com>
2025-09-09 15:30:16 +0200

    fix codestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,11 @@
 def roman_to_int(roman_string):
     result = 0
     temp = 0
-    roman_numeral = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    if roman_string == None or not isinstance(roman_string, str):
+    roman_numeral = {
+        'M': 1000, 'D': 500, 'C': 100,
+        'L': 50, 'X': 10, 'V': 5, 'I': 1
+    }
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     for roman_letter in reversed(roman_string):
         for key, value in roman_numeral.items():
