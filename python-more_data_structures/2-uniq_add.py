d901d767c6b41af775f88f4a6e63ab23adeda3ac
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 <11534@holbertonstudents.com>
2025-09-09 10:37:39 +0200

    add 2- files

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    verification_list = []
+    for i in my_list:
+        if i not in verification_list:
+            verification_list.append(i)
+            result += i
+    return(result) 
