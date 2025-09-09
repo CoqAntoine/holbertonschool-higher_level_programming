d9450315477fdcb218fd5316ce4687a85e7f6d1e
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 <11534@holbertonstudents.com>
2025-09-09 14:38:42 +0200

    add 10- files

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    name = None
+    best_score = 0
+    if bool(a_dictionary): 
+        for key, value in a_dictionary.items():
+            if value > best_score:
+                best_score = value
+                name = key
+    return name
