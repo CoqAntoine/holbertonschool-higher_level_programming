1c41b5c43ebc4bb00bdcf8c3ec7403fa9fbe0327
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 <11534@holbertonstudents.com>
2025-09-09 14:39:10 +0200

    fix trailing whitespace

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     name = None
     best_score = 0
-    if bool(a_dictionary): 
+    if bool(a_dictionary):
         for key, value in a_dictionary.items():
             if value > best_score:
                 best_score = value
