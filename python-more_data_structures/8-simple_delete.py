d87d77546d206160395a6fba46be670f9d2da403
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 <11534@holbertonstudents.com>
2025-09-09 14:06:07 +0200

    fix original dictionnary is also modified

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = {k: v for k, v in a_dictionary.items() if k != key}
-    return new_dict
+    temp = {k: v for k, v in a_dictionary.items() if k != key}
+    a_dictionary.clear()
+    a_dictionary.update(temp)
+    return a_dictionary
+
