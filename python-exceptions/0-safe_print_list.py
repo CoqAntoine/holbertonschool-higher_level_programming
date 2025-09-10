124247a34d7a124d8da9b067ec31c0b18f8988c1
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 <11534@holbertonstudents.com>
2025-09-10 11:24:06 +0200

    fix position of try:except:

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for element in my_list:
-            if count == x:
-               break
-            print(element, end='')
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
             count += 1
-        print()
-    except:
-        print("Error")
+        except IndexError:
+            break
+    print()
     return count
