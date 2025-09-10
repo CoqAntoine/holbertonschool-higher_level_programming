2649547fc2b562ca90a3b2e0495577e8ec51e404
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 <11534@holbertonstudents.com>
2025-09-10 11:57:54 +0200

    fix position of try:except:

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (TypeError, ValueError):
-            continue
-        except IndexError:
-            break
-    print()
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            except (ValueError, TypeError):
+                continue
+        print()
+    except IndexError:
+        raise
     return count
