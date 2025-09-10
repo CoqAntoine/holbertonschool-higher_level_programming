feabbf7e57a63a9976bed69a81fd10ab0b5a3871
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 <11534@holbertonstudents.com>
2025-09-10 11:22:08 +0200

    fix first line

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -8,6 +9,5 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         print()
     except:
-        print("Error" \
-        "")
+        print("Error")
     return count
