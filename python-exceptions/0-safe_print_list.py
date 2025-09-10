a8462126494d1163e9c8de8fbb914d99c6107af4
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 <11534@holbertonstudents.com>
2025-09-10 11:21:16 +0200

    add 0- files

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in my_list:
+            if count == x:
+               break
+            print(element, end='')
+            count += 1
+        print()
+    except:
+        print("Error" \
+        "")
+    return count
