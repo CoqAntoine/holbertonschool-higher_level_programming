e5afbf87dc08e614015de89f76281ea770b0fabf
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 <11534@holbertonstudents.com>
2025-09-12 12:10:25 +0200

    add 5- files

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module text_formatter
+
+This module provides a function to print a text with proper indentation 
+after certain punctuation marks (., ?, :).
+"""
+
+
+def text_indentation(text):
+    """
+    Prints the input text with a newline after '.', '?', or ':' characters.
+
+    Args:
+        text (str): The text to format and print.
+
+    Raises:
+        TypeError: If `text` is not a string.
+    """
+    do_not_space = False
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for character in text:
+        if do_not_space:
+            do_not_space = False
+            continue
+        if character == '.' or character == '?' or character == ':':
+            print(character)
+            do_not_space = True
+        else:
+            print(character, end="")
