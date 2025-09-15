6c07938862a87a3f82c962e315f7d393f1d6393c
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 <11534@holbertonstudents.com>
2025-09-15 11:11:47 +0200

    add some documentation to 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,10 +7,17 @@ It can be extended with attributes and methods for manipulating squares.
 class Square:
     """
     A class used to represent a Square.
+
+    Attributes:
+        (to be added later, e.g., `size` for the length of a side)
+
+    Methods:
+        (to be added later, e.g., `area` to compute the square's area)
     """
 
     def __init__(self):
         """
         Initialize a new Square instance.
+        Currently, no attributes are defined.
         """
         pass
