4e50d6ffb053d1af837e199a87cbb0f860ffbb33
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 <11534@holbertonstudents.com>
2025-09-15 14:28:15 +0200

    add 3- files

---
+++ python-classes/3-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module defines a `Square` class that represents a geometric square.
+It can be extended with attributes and methods for manipulating squares.
+"""
+
+
+class Square:
+    """
+    A class used to represent a Square.
+
+    Attributes:
+        size (int): The length of a side of the square.
+        Must be a non-negative integer.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int, optional): The length of a side of the square.
+                Defaults to 0. Must be a non-negative integer.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of a rectangle
+
+        Args:
+            size (int): The length of a side of the square.
+        """
+        return self.__size * self.__size
