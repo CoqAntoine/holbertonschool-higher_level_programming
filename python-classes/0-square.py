18756015f6436b8e5ddeed9540058c448fc2975e
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 <11534@holbertonstudents.com>
2025-09-15 11:11:08 +0200

    add 0- files

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a `Square` class that represents a geometric square.
+It can be extended with attributes and methods for manipulating squares.
+"""
+
+class Square:
+    """
+    A class used to represent a Square.
+    """
+
+    def __init__(self):
+        """
+        Initialize a new Square instance.
+        """
+        pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+Python programating : python classes
