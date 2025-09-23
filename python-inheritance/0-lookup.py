0203dda7fe8b6ba5325c19cfd215fa164e8f233b
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 <11534@holbertonstudents.com>
2025-09-23 11:06:47 +0200

    add 0- files

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: Any Python object.
+
+    Returns:
+        list: List of attributes and methods of the object.
+    """
+    return dir(obj)
