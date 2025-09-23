ff1af448ea18679b1fe71076c1ef74c9ac2c5c83
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 <11534@holbertonstudents.com>
2025-09-23 11:40:54 +0200

    add 4- files

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module: class_checker
+
+This module provides functions to check the type or class of an object,
+including inheritance relationships.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherits
+    (directly or indirectly) from the specified class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to compare against.
+
+    Returns:
+        bool: True if obj is an instance of a subclass of a_class
+        False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
