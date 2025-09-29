0dbf8b2c527162fbb1e7243643767b11467e3496
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 <11534@holbertonstudents.com>
2025-09-29 10:52:35 +0200

    add 0- files

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module reads a text file and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
+    f.closed
