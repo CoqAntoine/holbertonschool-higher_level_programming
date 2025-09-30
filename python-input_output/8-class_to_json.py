74c04b0bdf1818409c030ca3d08edc9c5d435237
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 <11534@holbertonstudents.com>
2025-09-30 14:19:05 +0200

    fix docstring

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module returns the dictionary description with 
-simple data structure (list, dictionary, string, integer and boolean) 
+This module returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object.
 """
 
@@ -10,7 +10,7 @@ import json
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with 
+    Returns the dictionary description with
     simple data structure
     """
     new_dict = obj.__dict__
