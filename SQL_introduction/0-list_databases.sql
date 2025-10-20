f85e4ffdd7080488d645efa0fa7cefce5aed298a
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 <11534@holbertonstudents.com>
2025-10-20 14:39:07 +0200

    Add task 1 file

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
--- Show the database
+-- Show the databases
 
 SHOW DATABASES;
---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Script that creates the database hbtn_0c_0 in the MySQL server
+-- Does not fail when hbtn_0c_0 already exists
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
