a6c568d613bfb69e634bc6c79645e1f7b9c5ae73
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 <11534@holbertonstudents.com>
2025-10-20 15:35:49 +0200

    Add task 9 file

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Script that creates a table called second_table in the given database
+-- and inserts multiple rows into it.
+-- Does not fail if the table already exists.
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
