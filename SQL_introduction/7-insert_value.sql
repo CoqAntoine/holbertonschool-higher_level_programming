fd1de373776ab3ce7c3b65ed1e90ba70133d4e4e
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 <11534@holbertonstudents.com>
2025-10-20 15:24:14 +0200

    Add task 7 file

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Script that inserts a new row in the table first_table
+-- The new row will have:
+--   id = 89
+--   name = 'Best School'
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
