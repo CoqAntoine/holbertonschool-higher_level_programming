b4575514a792503497687a870ef8b401b966c78b
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 <11534@holbertonstudents.com>
2025-10-21 15:24:25 +0200

    fix task 16 file

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Docstring
 
-SELECT t.title, g.name
+SELECT t.title, g.name AS genre
 FROM tv_shows t
 LEFT JOIN tv_show_genres tg ON t.id = tg.tv_show_id
 LEFT JOIN genres g ON tg.genre_id = g.id
-ORDER BY t.title ASC, g.name ASC;
+ORDER BY t.title ASC, genre ASC;
