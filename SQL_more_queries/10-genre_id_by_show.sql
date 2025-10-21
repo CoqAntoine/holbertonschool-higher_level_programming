ba053cdd5a460ec2dd1bd09ee0ae7e2c39285859
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 <11534@holbertonstudents.com>
2025-10-21 15:01:23 +0200

    add task 10 file

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Lists all tv shows that have at least one genre, showing title and genre_id
+-- Sorted by tv_shows.title and genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
