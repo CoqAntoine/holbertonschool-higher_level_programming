4ccc5f1b3db12641159441118949499c6bc547b7
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 <11534@holbertonstudents.com>
2025-10-21 14:33:52 +0200

    Add task 1 file

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates the user user_0d_1 with all privileges on the server.
+-- The user_0d_1 password is set to user_0d_1_pwd
+-- Does not fail if the user user_0d_1 already exists
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
