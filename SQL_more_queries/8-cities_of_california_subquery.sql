4a97340975f3d57b469fa22d71445e8147b136c4
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 <11534@holbertonstudents.com>
2025-10-21 14:57:22 +0200

    fix situation

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Lists all cities in the state of California
 -- Results are sorted in ascending order by cities.id.
 
-SELECT cities.id, cities.name, cities.state_id
+SELECT id, name
 FROM cities
-WHERE states.name = 'California'
-ORDER BY cities.id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
