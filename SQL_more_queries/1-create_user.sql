76e737fd9e6c1a8f4014d678e602bb13fbdc0460
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 <11534@holbertonstudents.com>
2025-10-21 14:35:35 +0200

    fix "if not exists" and "grant all privileges"

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,5 @@
 -- The user_0d_1 password is set to user_0d_1_pwd
 -- Does not fail if the user user_0d_1 already exists
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
