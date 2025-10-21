6b6d147e153155993e2ba2a9b0e5aad1f94bed01
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 <11534@holbertonstudents.com>
2025-10-21 14:38:51 +0200

    fix docstrings

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- Creates the user user_0d_1 with all privileges on the server.
--- The user_0d_1 password is set to user_0d_1_pwd
--- Does not fail if the user user_0d_1 already exists
 
+-- Create user user_0d_1 if it doesn't already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
