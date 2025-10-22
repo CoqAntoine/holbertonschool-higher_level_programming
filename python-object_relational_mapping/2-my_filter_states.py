2832833df6a3e00ca4884a499c1e127de7c89ff3
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 <11534@holbertonstudents.com>
2025-10-22 15:58:49 +0200

    fix BINARY and pypy

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Create SQL query using format with user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name)
+    query = "SELECT * FROM states WHERE name \
+            LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
 
     # Execute the query
     cursor.execute(query)
