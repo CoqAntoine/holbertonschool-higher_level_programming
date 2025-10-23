19f6812d968e9754fd094158d6ced6d5bdecda93
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 <11534@holbertonstudents.com>
2025-10-23 16:10:58 +0200

    fix pypy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,13 +11,12 @@ Base = declarative_base()
 class State(Base):
     """
     State class that represents the states table in MySQL
-    
+
     Attributes:
         id: Auto-generated unique integer, primary key, cannot be null
         name: String with maximum 128 characters, cannot be null
     """
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    
