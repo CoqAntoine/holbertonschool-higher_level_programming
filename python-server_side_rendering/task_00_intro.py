1f3d141d01a945fa431d6c1b9dff1570d9db536f
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 <11534@holbertonstudents.com>
2025-11-12 14:06:02 +0100

    Add commentaries

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,4 +1,10 @@
 def generate_invitations (template, attendees):
+    """
+    Generate invitations files from a template and a list of attendees
+    Output files are names sequentially (output_1.txt, output_2.txt,...)
+    """
+
+    # Verify the type of parameters
     if not isinstance(template, str):
         print("Error: Invalid input type for template. Expected a string.")
         return
@@ -6,6 +12,7 @@ def generate_invitations (template, attendees):
         print("Error: Invalid input type for attendees. Expected a list of dictionnaries.")
         return
     
+    # Verify the parameters are not empty
     if not template.strip():
         print("Template is empty, no output files generated.")
         return
@@ -13,7 +20,9 @@ def generate_invitations (template, attendees):
         print("No data provided, no output files generated.")
         return
     
+    # Fill each invitation
     for i, person in enumerate(attendees, start=1):
+        # Use dict.get() with fallback "N/A", also handle None values
         filled = template.format(
             name = person.get("name", "N/A") or "N/A",
             event_title=person.get("event_title", "N/A") or "N/A",
@@ -21,6 +30,7 @@ def generate_invitations (template, attendees):
             event_location=person.get("event_location", "N/A") or "N/A"
         )
 
+        # Create output files
         output_filename = f"output_{i}.txt"
         try:
             with open(output_filename, "w") as file:
