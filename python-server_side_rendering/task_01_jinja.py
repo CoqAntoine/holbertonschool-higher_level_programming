89084b999983cc59bba226bb45ffb929a8aefab6
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 <11534@holbertonstudents.com>
2025-11-13 13:36:59 +0100

    Fix def name

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -11,7 +11,7 @@ def about():
     return render_template('about.html')
 
 @app.route('/contact')
-def about():
+def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
